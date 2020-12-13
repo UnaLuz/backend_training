0f9770c0a963c8c6c0bae30653867e973b9b23f7
UnaLuz <luz.cucarella97@gmail.com>
2020-12-13 19:07:53 -0300

    Usando middlewares para procesar datos Agregué el middleware 'morgan'

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const { json } = require('express');
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+// Middlewares, deben estar antes de las rutas para que funcionen
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.send('GET petition recieved');
+});
+
+app.all('/gr8', (req, res, next) => {
+    console.log("You're doing great");
+    next();
 });
 
 app.get('/user', (req, res) => {
@@ -32,6 +39,10 @@ app.put('/contact', (req, res) => {
 
 app.delete('/test', (req, res) =>{
     res.send('<h1>DELETE request recieved</h1>');
+});
+
+app.delete('/user/:userId', (req, res) =>{
+    res.send(`User ${req.params.userId} deleted`);
 });
 
 app.listen(5000, () => {
